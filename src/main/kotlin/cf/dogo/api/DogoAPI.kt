8e1e85331aca061512f9fa3de51238d40b2d99fd
NathanPB <nathan.pbombana@gmail.com>
2018-06-21 01:46:38 -0300

    Improvements on reconnection

--- src/main/kotlin/cf/dogo/api/DogoAPI.kt
+++ src/main/kotlin/cf/dogo/api/DogoAPI.kt
@@ -15,5 +15,11 @@ fun main(args : Array<String>){
     var ata = if(args.size > 1)  arrayOf(args[0], args[1]) else arrayOf("localhost", "4676")
     DogoAPI.connection = Client(ata[0], ata[1].toInt(), "Client")
 
-    runApplication<DogoAPI>(*args)
+    //yep, that == true is required
+    if(DogoAPI.connection?.isAvailabe() == true) {
+        runApplication<DogoAPI>(*args)
+    } else {
+        Thread.sleep(30000)
+        main(args)
+    }
 }
