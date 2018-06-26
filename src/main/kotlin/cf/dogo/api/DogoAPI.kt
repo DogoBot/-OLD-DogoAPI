5d4b21db507e0ffb5546642438f7fc79449fd36f
NathanPB <nathan.pbombana@gmail.com>
2018-06-25 22:17:24 -0300

    Gambiarra fixed

--- src/main/kotlin/cf/dogo/api/DogoAPI.kt
+++ src/main/kotlin/cf/dogo/api/DogoAPI.kt
@@ -1,21 +1,22 @@
 package cf.dogo.api
 
+import cf.dogo.server.bridge.Client
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 open class DogoAPI{
     companion object {
-        var connection : Client? = null
+        var connection : cf.dogo.server.bridge.Client? = null
     }
 }
 
 fun main(args : Array<String>) {
     var ata = if(args.size > 1)  arrayOf(args[0], args[1]) else arrayOf("localhost", "4676")
-    DogoAPI.connection = Client(ata[0], ata[1].toInt())
+    DogoAPI.connection = Client(args[0], args[1].toInt(), "Client")
 
     //yep, that == true is required
-    if(DogoAPI.connection?.isAvailable == true) {
+    if(DogoAPI.connection?.isAvailabe() == true) {
         runApplication<DogoAPI>(*args)
     } else {
         Thread.sleep(30000)
