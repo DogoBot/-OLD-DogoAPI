41375739ed107926c3c5855d950f5f23d6b5d016
NathanPB <nathan.pbombana@gmail.com>
2018-06-25 23:27:11 -0300

    Bug Fix

--- src/main/kotlin/cf/dogo/api/DogoAPI.kt
+++ src/main/kotlin/cf/dogo/api/DogoAPI.kt
@@ -14,6 +14,7 @@ open class DogoAPI{
 fun main(args : Array<String>) {
     var ata = if(args.size > 1)  arrayOf(args[0], args[1]) else arrayOf("localhost", "4676")
     DogoAPI.connection = Client(ata[0], ata[1].toInt(), "Client")
+    DogoAPI.connection?.connect()
 
     //yep, that == true is required
     if(DogoAPI.connection?.isAvailabe() == true) {
